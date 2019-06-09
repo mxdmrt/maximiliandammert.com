1d2f773e924c5f872065dc66d06bc5e433e07551
madammert <34488580+madammert@users.noreply.github.com>
2019-06-09 11:23:48 +0200

    add randomColor.js

---
+++ randomColor.js
@@ -0,0 +1,31 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function getCorrectTextColor(hex) {
+    threshold = 128; /* about half of 256. Lower threshold equals more dark text on dark background  */
+
+    hRed = hexToR(hex);
+    hGreen = hexToG(hex);
+    hBlue = hexToB(hex);
+
+    function hexToR(h) { return parseInt((cutHex(h)).substring(0, 2), 16) }
+    function hexToG(h) { return parseInt((cutHex(h)).substring(2, 4), 16) }
+    function hexToB(h) { return parseInt((cutHex(h)).substring(4, 6), 16) }
+    function cutHex(h) { return (h.charAt(0) == "#") ? h.substring(1, 7) : h }
+
+    cBrightness = ((hRed * 299) + (hGreen * 587) + (hBlue * 114)) / 1000;
+    if (cBrightness > threshold) { return "#000000"; } else { return "#ffffff"; }
+}
+
+function setRandomColor() {
+    bgColor = getRandomColor();
+    $(":root").css("--colorBg", bgColor);
+    $(":root").css("--colorTxt", getCorrectTextColor(bgColor));
+}
