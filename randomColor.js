65287c3ddcb847a02e59cfda14efe1ecbab474fb
madammert <34488580+madammert@users.noreply.github.com>
2019-06-21 07:42:08 +0200

    clean up and rename files

--- randomColor.js
+++ randomColor.js
@@ -1,59 +1,7 @@
-function getRandomColor() {
-	var letters = '0123456789ABCDEF';
-	var color = '#';
-	for (var i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
+function randomHex() {
+	const hexMax = 256 * 256 * 256;
+	return '#' + Math.floor(Math.random() * hexMax).toString(16).toUpperCase().padStart(6, '0');
 }
-
-// const color = [
-//     '#0000ff',
-//     '#000000',
-//     '#ffffff'
-// ];
-
-// function getRandomColor(color) {
-//     return color[Math.floor(Math.random() * color.length)];
-// }
-
-// const fonts = [
-//     'IBM Plex Serif',
-//     'IBM Plex Sans',
-//     'IBM Plex Mono',
-//     // 'Volkhov',
-//     // 'Alegreya',
-//     // 'Fira Sans Condensed',
-//     // 'Fira Sans',
-//     // 'Barlow',
-//     // 'Karla',
-//     // 'Muli',
-//     // 'PT Serif',
-//     // 'Roboto Mono',
-//     // 'Libre Franklin',
-//     // 'Lora',
-//     // 'Poppins',
-//     // 'Merriweather',
-//     // 'Raleway',
-//     // 'Montserrat',
-//     // 'Open Sans'
-// ];
-
-// function randomFont(fonts) {
-//     return fonts[Math.floor(Math.random() * fonts.length)];
-// }
-
-// console.log(randomFont(fonts));
-
-// const size = [
-//     '1rem',
-//     '1.25rem',
-//     '1.5rem',
-// ];
-
-// function randomSize(size) {
-//     return size[Math.floor(Math.random() * size.length)];
-// }
 
 function getCorrectTextColor(hex) {
 
@@ -73,10 +21,7 @@ function getCorrectTextColor(hex) {
 }
 
 function setRandomColor() {
-	bgColor = getRandomColor();
+	bgColor = randomHex();
 	$(":root").css("--colorBg", bgColor);
 	$(":root").css("--colorTxt", getCorrectTextColor(bgColor));
-	// $("body").css("font-family", randomFont(fonts));
-	// $(":root").css("--baseValue", randomSize(size));
 }
-
